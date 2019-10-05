341c8aa632abf921085b524966856347435d4012
DIVYAM GUPTA <divyam.gupta_cs18@gla.ac.in>
2019-10-05 15:59:47 +0530

    creating execution class
    Signed-off-by: DIVYAM GUPTA <divyam.gupta_cs18@gla.ac.in>

--- src/String2.java
+++ src/String2.java
@@ -1,5 +1,20 @@
+import java.util.Scanner;
+
 public class String2 {
-    public void printSubString(String str, int index) {
+    public void SubString(String str, int index) {
         System.out.println(str.substring(index));
+    }
+}
+
+class Execution2 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter string: ");
+        String str = scan.nextLine().trim();
+        System.out.println("Enter index: ");
+        int index = scan.nextInt();
+        scan.close();
+        String2 obj = new String2();
+        obj.SubString(str, index);
     }
 }
