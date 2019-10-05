5182cfce89e4e0c08f35ed6542ded0b7a83e3d8a
DIVYAM GUPTA <divyam.gupta_cs18@gla.ac.in>
2019-10-05 15:46:38 +0530

    creating string class and printing index seperated by tab
    Signed-off-by: DIVYAM GUPTA <divyam.gupta_cs18@gla.ac.in>

---
+++ src/String1.java
@@ -0,0 +1,12 @@
+public class String1 {
+    public void String(String S) {
+        int len = S.length();
+        System.out.print("index: ");
+        for (int i = 0; i < len; i++) {
+            System.out.print(i + "\t");
+        }
+        System.out.println();
+
+
+    }
+}
