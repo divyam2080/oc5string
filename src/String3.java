ee7612d7205662bbabf15ae3d6d7b5dc5a484f34
DIVYAM GUPTA <divyam.gupta_cs18@gla.ac.in>
2019-10-05 16:11:29 +0530

    executing the class for relacement of vowels by calling object
    Signed-off-by: DIVYAM GUPTA <divyam.gupta_cs18@gla.ac.in>

--- src/String3.java
+++ src/String3.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class String3 {
     public void Replacing(String str) {
         char[] arr = new char[]{'A', 'E', 'I', 'O', 'U', 'a', 'e', 'i', 'o', 'u'};
@@ -5,5 +7,15 @@ public class String3 {
             str = str.replace(arr[i], '*');
         }
         System.out.println(str);
+    }
+}
+
+class Execution3 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter string: ");
+        String str = scan.nextLine().trim();
+        String3 obj = new String3();
+        obj.Replacing(str);
     }
 }
